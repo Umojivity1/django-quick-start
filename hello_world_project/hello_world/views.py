bfca7a9452b8798f486545f8ff378941804a8f50
Umojivity1 <admin@umojivity.com>
2015-08-03 11:40:12 +0000

    added some views to the hello_world application views

--- hello_world_project/hello_world/views.py
+++ hello_world_project/hello_world/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Context, loader
+from datetime import datetime
 
 # Create your views here.
 def index(request):
@@ -9,3 +11,8 @@ def about(request):
     return HttpResponse(
         "Here is the About Page. Go back home? <a href='/'>Home</a>"
     )
+
+def better(request):
+    t = loader.get_template('betterhello.html')
+    c = Context({'current_time': datetime.now(), })
+    return HttpResponse(t.render(c))
