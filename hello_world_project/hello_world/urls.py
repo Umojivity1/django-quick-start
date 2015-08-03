c240099cda5df629ee0e311b1578d107cee197cf
Umojivity1 <admin@umojivity.com>
2015-08-03 11:39:02 +0000

    mapping the hello_world application views to specific urls

--- hello_world_project/hello_world/urls.py
+++ hello_world_project/hello_world/urls.py
@@ -4,5 +4,6 @@ from hello_world import views
 urlpatterns = patterns(
     'hello_world.views',
     url(r'^$', views.index, name='index'),
-    url('^about/', views.about, name='about'),
+    url(r'^about/', views.about, name='about'),
+    url(r'^better/', views.better, name='better'),
 )
